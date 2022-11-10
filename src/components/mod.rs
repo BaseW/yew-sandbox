39fe0077109fc93ec41043b3cb7d6499688c6319
wada <wadayugo@keio.jp>
2022-11-10 23:46:29 +0900

    use first component

---
+++ src/components/mod.rs
@@ -0,0 +1,26 @@
+use yew::prelude::*;
+
+pub struct App {}
+
+impl Component for App {
+    type Message = ();
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self {}
+    }
+
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        false
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        // This gives us a component's "`Scope`" which allows us to send messages, etc to the component.
+        let link = ctx.link();
+        html! {
+            <div>
+                <h1>{ "Hello World!" }</h1>
+            </div>
+        }
+    }
+}
